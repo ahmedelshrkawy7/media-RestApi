fde67b2c1f5c12f768be124cca571327eb20143c
ahmedelshrkawy7 <116393265+ahmedelshrkawy7@users.noreply.github.com>
2024-08-08 09:37:03 +0300

    new functions added

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,45 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // register
 
-router.get("/register", async (req, res) => {
-  const user = await new User({
-    username: "jhon",
-    email: "ahmed @yakoo.com",
-    password: "123456",
-  });
-  await user.save();
-  res.send("ok");
+router.post("/register", async (req, res) => {
+  console.log("🚀 ~ router.post ~ req:", req.body);
+
+  try {
+    //// generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = await new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.log("an error happend");
+  }
+});
+
+////////////////  Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    console.log("🚀 ~ router.post ~ user:", user);
+    !user && res.status(404).send("user not found");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(404).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
